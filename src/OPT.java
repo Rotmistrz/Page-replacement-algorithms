3e802cbc344f03f81e50593dd3ea14baa22b6799
Filip <filip.markiewicz96@gmail.com>
2017-05-03 20:42:04 +0200

    Opt algorithm and finish

--- src/OPT.java
+++ src/OPT.java
@@ -1,31 +1,112 @@
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.ListIterator;
 
-public class OPT extends PageReplacementAlgorithm {
-
+public class OPT {
+	Memory physicalMemory;
+	Memory virtualMemory;
+	int missErrors = 0;
+	
+	LinkedList<Integer> requests;
+	
 	public OPT(Memory physicalMemory, Memory virtualMemory) {
-		super(physicalMemory, virtualMemory);
+		this.physicalMemory = physicalMemory;
+		this.virtualMemory = virtualMemory;
+		this.requests = new LinkedList<Integer>();
 	}
-
-	@Override
-	protected MemoryPage throwOutPage() {
-		ListIterator<MemoryPage> it = physicalMemory.listIterator();
+	
+	public OPT setRequests(LinkedList<Integer> list) {
+		this.requests = list;
 		
-		MemoryPage oldest = it.next();
-		MemoryPage current;
+		return this;
+	}
+	
+	public int run(LinkedList<Integer> requests) {
+		missErrors = 0;
+		
+		setRequests(requests);
+		ListIterator<Integer> it = this.requests.listIterator();
+		
+		int n = 0;
+		int current;
+		MemoryPage thrownOut;
+		MemoryPage page;
 		
 		while(it.hasNext()) {
 			current = it.next();
 			
-			if(current.lastRequestTime < oldest.lastRequestTime) {
-				oldest = current;
+			if(!physicalMemory.isInMemory(current)) {
+				missErrors++;
+				//System.out.println(current + " out of physical memory; missErrors = " + missErrors);
+				
+				try {
+					thrownOut = throwOutPage(n);
+					page = virtualMemory.getById(current);
+					
+					if(page instanceof NoMemoryPage) {
+						throw new PageDoesntExistException();
+					} else {
+						Date currentTime = new Date();
+						page.locatedInMemoryTime = currentTime.getTime();
+						physicalMemory.add(page);
+						virtualMemory.remove(page);
+						virtualMemory.add(thrownOut);
+					}
+				} catch(PageDoesntExistException pdee) {
+					System.out.println("Page #" + current + " doesn't exist in any memory. Possible incorrect result of simulation.");
+				}
+			} else {
+				//System.out.println(current + " in physical memory.");
+			}
+			
+			Date currentTime = new Date();
+			
+			page = physicalMemory.getById(current);
+			page.lastRequestTime = currentTime.getTime();
+			
+			n++;
+		}
+		
+		return missErrors;
+	}
+
+	protected MemoryPage throwOutPage(int requestId) {
+		ListIterator<MemoryPage> it;
+		ListIterator<Integer> requestIt = requests.listIterator(requestId);
+		
+		int foundId;
+		int currentRequestId;
+		MemoryPage unnecessary;
+		MemoryPage current;
+		
+		Memory physicalMemoryClone = PageReplacementAlgorithm.copyMemory(physicalMemory);
+		
+		ArrayList<MemoryPage> necessaryPages = new ArrayList<MemoryPage>();
+		int length = physicalMemory.size() - 1;
+		
+		while(requestIt.hasNext() && necessaryPages.size() < length) {
+			currentRequestId = requestIt.next();
+			it = physicalMemoryClone.listIterator();
+			
+			while(it.hasNext()) {
+				current = it.next();
+				
+				if(current.id == currentRequestId) {
+					necessaryPages.add(current);
+					physicalMemoryClone.remove(current);
+					break;
+				}
 			}
 		}
 		
-		physicalMemory.remove(oldest);
+		it = physicalMemoryClone.listIterator();
+		unnecessary = it.next();
+		foundId = unnecessary.id;
 		
-		return oldest;
+		unnecessary = physicalMemory.getById(foundId);
+		physicalMemory.remove(unnecessary);
+		
+		return unnecessary;
 	}
-
 }
--- src/PageReplacementAlgorithms.java
+++ src/PageReplacementAlgorithms.java
@@ -3,26 +3,28 @@ import java.util.LinkedList;
 public class PageReplacementAlgorithms {
 
 	public static void main(String[] args) throws Exception {
-		Memory physicalMemory = new Memory(20);
-		Memory virtualMemory = new Memory(40);
+		Memory physicalMemory = new Memory(10);
+		Memory virtualMemory = new Memory(20);
 		
-		for(int i = 0; i < 20; i++) {
+		for(int i = 0; i < 10; i++) {
 			physicalMemory.add(new MemoryPage());
 		}
 		
-		for(int i = 0; i < 40; i++) {
+		for(int i = 0; i < 20; i++) {
 			virtualMemory.add(new MemoryPage());
 		}
 
-			LinkedList<Integer> requests = ListsFactory.createIntegerList("requests-2.txt");
-			
-			FIFO fifo = new FIFO(PageReplacementAlgorithm.copyMemory(physicalMemory), PageReplacementAlgorithm.copyMemory(virtualMemory));
-			LRU lru = new LRU(PageReplacementAlgorithm.copyMemory(physicalMemory), PageReplacementAlgorithm.copyMemory(virtualMemory));
-			RAND rand = new RAND(PageReplacementAlgorithm.copyMemory(physicalMemory), PageReplacementAlgorithm.copyMemory(virtualMemory));
-			
-			System.out.println("FIFO: " + fifo.run(requests));
-			System.out.println("LRU: " + lru.run(requests));
-			System.out.println("RAND: " + rand.run(requests));
+		LinkedList<Integer> requests = ListsFactory.createIntegerList("requests.txt");
+		
+		FIFO fifo = new FIFO(PageReplacementAlgorithm.copyMemory(physicalMemory), PageReplacementAlgorithm.copyMemory(virtualMemory));
+		LRU lru = new LRU(PageReplacementAlgorithm.copyMemory(physicalMemory), PageReplacementAlgorithm.copyMemory(virtualMemory));
+		RAND rand = new RAND(PageReplacementAlgorithm.copyMemory(physicalMemory), PageReplacementAlgorithm.copyMemory(virtualMemory));
+		OPT opt = new OPT(PageReplacementAlgorithm.copyMemory(physicalMemory), PageReplacementAlgorithm.copyMemory(virtualMemory));
+		
+		System.out.println("FIFO: " + fifo.run(requests));
+		System.out.println("LRU: " + lru.run(requests));
+		System.out.println("RAND: " + rand.run(requests));
+		System.out.println("OPT: " + opt.run(requests));
 		
 	}
 }
